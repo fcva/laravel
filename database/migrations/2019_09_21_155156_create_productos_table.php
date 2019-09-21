d581c5bac1d5dda94eb54212e7858b7e3f160fd5
Freddy VA <fcva.14@gmail.com>
2019-09-21 11:34:13 -0500

    table productos

--- database/migrations/2019_09_21_155156_create_productos_table.php
+++ database/migrations/2019_09_21_155156_create_productos_table.php
@@ -15,7 +15,16 @@ class CreateProductosTable extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->string('codigo', 120)->nullable();
+            $table->string('nombre', 160);
+            $table->string('marca', 160);
+            $table->string('modelo', 160);
+            $table->string('descripcion', 180)->nullable();
+            $table->string('observacion', 180)->nullable();
+            $table->boolean('es_activo')->default(true);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
