f4e0e111ebe5b6fca1733343a05d4f237d0a1359
Freddy VA <fcva.14@gmail.com>
2019-09-21 11:23:27 -0500

    Creacion del diseño

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,10 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('observacion', 180)->nullable();
+            $table->boolean('es_activo')->default(true);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
